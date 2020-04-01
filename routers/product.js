4ac65a5a69da8c62f06e633fd3dbf7f4cbe8c715
Ashirbad Subudhi <ashirbad5subudhi@gmail.com>
2020-04-01 20:48:44 +0530

    Update product.js

--- routers/product.js
+++ routers/product.js
@@ -7,7 +7,7 @@ const productController = require('../controllers/productController');
 router.post('/create/', productController.create);
 router.get('/', productController.allProducts);
 router.delete('/:id', productController.delete);
-router.get("/:id/update_quantity/", productController.update);
+router.post("/:id/update_quantity/", productController.update);
 
 //exporting module
 module.exports = router;
